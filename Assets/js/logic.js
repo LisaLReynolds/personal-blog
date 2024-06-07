f700fe2c91b27ccd77f87dc02afa720f6c6cee61
Bowser <lisalreynolds2015@gmail.com>
2024-06-07 15:33:26 -0600

    added pseudo code to logic.js

---
+++ Assets/js/logic.js
@@ -0,0 +1,20 @@
+//declare values
+
+//in logic.js
+//create object
+//username: declared value
+//title: declared value
+//content: declared value
+
+//store object in local storage as JSON stringify
+//save as array, push to array new entries
+
+//event listener for submit button redirect to new page and store object
+//reset values??
+
+//In blog.js
+//second page
+//call function to get from storage object
+//parse JSON
+//render info
+//loop for all stored objects
