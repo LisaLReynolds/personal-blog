e0d270bb78271cf2f0f9fa4b0657e91207c9a149
Bowser <lisalreynolds2015@gmail.com>
2024-06-06 22:20:41 -0600

    fixed margin in header, updated class names

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,12 +1,12 @@
 // Access toggle switch HTML element
-const themeSwitcher = document.querySelector("#theme-switcher");
+const modeSwitcher = document.querySelector("#mode-switcher");
 const container = document.querySelector(".container");
 
 // Set default mode to light
 let mode = "light";
 
 // Listen for a click event on toggle switch
-themeSwitcher.addEventListener("click", function () {
+modeSwitcher.addEventListener("click", function () {
   // If mode is light, apply dark background
   if (mode === "light") {
     mode = "dark";
