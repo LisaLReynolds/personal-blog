5184355cf2f3877a2abc145a52200c403ceb440e
Bowser <lisalreynolds2015@gmail.com>
2024-06-07 21:19:36 -0600

    resolved submit button redirect issue

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,29 +1,29 @@
 //declare values
-const userName = document.getElementById("#user-name");
-const blogTitle = document.getElementById("#blog-title");
-const blogContent = document.getElementById("#blog-content");
+const userName = document.getElementById("user-name");
+const blogTitle = document.getElementById("blog-title");
+const blogContent = document.getElementById("blog-content");
+const submitButton = document.getElementById("submit-button");
 
 //add event listener for submit button
-document
-  .getElementById("submitButton")
-  .addEventlistener("click", function (event) {
-    event.preventDefault(); //prevent default form submission
-    //create object
-    //username: declared value
-    //title: declared value
-    //content: declared value
-    const userInput = {
-      userName: userName.value,
-      blogTitle: blogTitle.value,
-      blogContent: blogContent.value,
-    };
 
-    //store object in local storage as JSON stringify
-    //save as array, push to array new entries
-    localStorage.setItem("userInput", JSON.stringify(userInput));
+submitButton.addEventListener("click", function (event) {
+  event.preventDefault(); //prevent default form submission
+  //create object
+  //username: declared value
+  //title: declared value
+  //content: declared value
+  const userInput = {
+    userName: userName.value,
+    blogTitle: blogTitle.value,
+    blogContent: blogContent.value,
+  };
 
-    redirectToNewPage();
-  });
+  //store object in local storage as JSON stringify
+  //save as array, push to array new entries
+  localStorage.setItem("userInput", JSON.stringify(userInput));
+
+  redirectToNewPage();
+});
 //event listener for submit button redirect to new page and store object
 
 function redirectToNewPage() {
