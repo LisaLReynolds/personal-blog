6c3f4b64e1c9b1d470965f786dc7ca9e78e9344d
Bowser <lisalreynolds2015@gmail.com>
2024-06-09 12:06:03 -0600

    fixed back button code in blog.js

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       entryDiv.classList.add("entry");
 
       const userName = document.createElement("h3");
-      userName.textContent = entry.userName;
+      userName.textContent = "Posted by: " + entry.userName;
 
       const blogTitle = document.createElement("h2");
       blogTitle.textContent = entry.blogTitle;
@@ -30,24 +30,12 @@ document.addEventListener("DOMContentLoaded", function () {
       entryDiv.appendChild(blogContent);
 
       // Append the entry div to the document body
-      document.body.appendChild(entryDiv);
+      const blogEntries = document.getElementById("blog-entries");
+      blogEntries.appendChild(entryDiv);
     });
   }
 });
 
-//from billy's code:
-// window.onload = function () {
-//   const blogEntries = document.getElementById("blog-entries");
-//   const userInput = JSON.parse(localStorage.getItem("userInput")) || [];
-//   blogEntries.forEach((post) => {
-//     const divEl = document.createElement("div");
-//     divEl.classList.add("blog-div");
-//     divElv.innerHTML = `
-//             <h2>Title :${post.blogTitle}</h2>
-//             <h3>User name: ${post.userName}</h3>
-//             <p>${post.blogContent}</p>
-//             <hr>
-//         `;
-//     blogContent.appendChild(divEl);
-//   });
-// };
+function goBack() {
+  window.location.href = "./index.html";
+}
